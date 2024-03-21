d10e85022b1e3e9abfb7a908926a1a3f06f7100d
Zach Arnett <88991823+Zach-Arnett@users.noreply.github.com>
2024-03-21 17:05:49 -0400

    Adjusted CSS structure, added template to all pages

--- javascript/overview.js
+++ javascript/overview.js
@@ -1,51 +1,38 @@
-//----------Option Select----------
-//Get references to the buttons
+//---------- Option Select ----------
+// Get references to the buttons
 var button1 = document.getElementById("body-only");
 var button2 = document.getElementById("body-extra");
 // Get preview image to be altered
 var previewImage = document.getElementById('preview-image');
 
-
-//Function to handle button click
-function handleButtonClick(clickedButton) {
-  //Deselect both buttons
-  button1.classList.remove("selected");
-  button2.classList.remove("selected");
-  //Select the clicked button
-  clickedButton.classList.add("selected");
-}
-
-//Add click event listeners to buttons
+// Button Select Behavior
 button1.addEventListener("click", function() {
-  handleButtonClick(button1);
+  button2.classList.remove("selected");
+  button1.classList.add("selected");
   previewImage.src = "images/image_body_front.png";
 });
 
 button2.addEventListener("click", function() {
-  handleButtonClick(button2);
+  button1.classList.remove("selected");
+  button2.classList.add("selected");
   previewImage.src = "images/image_camera_front.png";
-  
 });
 
 
 
-//----------Extend Field Set----------
+//---------- Extend Field Set ----------
 var extendButton = document.getElementById("extend-button");
 var extendField = document.getElementById("extend-field");
 
-//toggle if the field set is extended
-function extend(extendField) {
-    if(extendField.classList == "not-extended"){
-        extendField.classList.remove("not-extended");
-        extendField.classList.add("extended");
-    } else{
-        extendField.classList.remove("extended");
-        extendField.classList.add("not-extended");
-    }
-}
-
+// Toggle if the field set is extended
 extendButton.addEventListener("click", function() {
-    extend(extendField);
+  if(extendField.classList.contains("not-extended")){
+    extendField.classList.remove("not-extended");
+    extendField.classList.add("extended");
+} else {
+    extendField.classList.remove("extended");
+    extendField.classList.add("not-extended");
+}
 });
 
 
