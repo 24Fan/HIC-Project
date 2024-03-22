46e025b7e1e73acd8ac2435837772ad61f820fb3
Zach Arnett <88991823+Zach-Arnett@users.noreply.github.com>
2024-03-21 22:07:38 -0400

    Updated Overview to reflect changing camera options

--- javascript/overview.js
+++ javascript/overview.js
@@ -1,39 +1,58 @@
 //---------- Option Select ----------
-// Get references to the buttons
+// Get option buttons
 var button1 = document.getElementById("body-only");
 var button2 = document.getElementById("body-extra");
+
+// Get descriptions to be altered
+var bodyDetails = document.getElementById('body-details');
+var cameraDetails = document.getElementById('camera-details');
+
+// Get prices to be altered
+var bodyPrice = document.getElementById('body-price');
+var cameraPrice = document.getElementById('camera-price');
+
 // Get preview image to be altered
 var previewImage = document.getElementById('preview-image');
 
 // Button Select Behavior
-button1.addEventListener("click", function() {
+function handleButtonClick(selectedButton, hiddenDetails, visibleDetails, hiddenPrice, visiblePrice, imageSrc) {
+  button1.classList.remove("selected");
   button2.classList.remove("selected");
-  button1.classList.add("selected");
-  previewImage.src = "images/image_body_front.png";
+  selectedButton.classList.add("selected");
+
+  hiddenDetails.classList.add("hidden");
+  visibleDetails.classList.remove("hidden");
+
+  hiddenPrice.classList.add("hidden");
+  visiblePrice.classList.remove("hidden");
+
+  extendField.classList.remove("camera-extended", "body-extended");
+  extendField.classList.add("not-extended");
+
+  previewImage.src = imageSrc;
+}
+
+button1.addEventListener("click", function() {
+  handleButtonClick(button1, cameraDetails, bodyDetails, cameraPrice, bodyPrice, "images/image_body_front.png");
 });
 
 button2.addEventListener("click", function() {
-  button1.classList.remove("selected");
-  button2.classList.add("selected");
-  previewImage.src = "images/image_camera_front.png";
+  handleButtonClick(button2, bodyDetails, cameraDetails, bodyPrice, cameraPrice, "images/image_camera_front.png");
 });
-
 
 
 //---------- Extend Field Set ----------
 var extendButton = document.getElementById("extend-button");
 var extendField = document.getElementById("extend-field");
 
-// Toggle if the field set is extended
 extendButton.addEventListener("click", function() {
-  if(extendField.classList.contains("not-extended")){
-    extendField.classList.remove("not-extended");
-    extendField.classList.add("extended");
-} else {
-    extendField.classList.remove("extended");
-    extendField.classList.add("not-extended");
-}
+  var isNotExtended = extendField.classList.contains("not-extended");
+  extendField.classList.remove("camera-extended", "body-extended", "not-extended");
+
+  if (cameraDetails.classList.contains("hidden")) {
+      extendField.classList.add(isNotExtended ? "body-extended" : "not-extended");
+  } else {
+      extendField.classList.add(isNotExtended ? "camera-extended" : "not-extended");
+  }
 });
-
-
 
